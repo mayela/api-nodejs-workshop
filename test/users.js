8091c7b25e83c504105173f343f41ba54c2aad10
Maricela Sanchez <mayela0x14@gmail.com>
2018-08-03 13:23:31 -0500

    Add user tests

--- test/users.js
+++ test/users.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = 'test';
 
-let mongoose = require("mongoose");
 let User = require('../models/user');
 
 //Require the dev-dependencies
@@ -22,7 +21,7 @@ describe('Users', () => {
   */
   describe('/GET users', () => {
       it('it should GET all the users', (done) => {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .get('/users')
             .end((err, res) => {
                 res.should.have.status(200);
@@ -33,4 +32,85 @@ describe('Users', () => {
       });
   });
 
+  describe('/POST user', () => {
+    it('it should POST an user', (done) => {
+      let user = new User({
+          username: "fulanito",
+          first_name: "Fulano",
+          last_name: "de Tal"
+      });
+      chai.request('http://localhost:3000')
+          .post('/users')
+          .send(user)
+          .end((err, res) => {
+              res.should.have.status(201);
+              res.body.should.be.a('object');
+            done();
+          });
+    });
+
+    });
+
+    describe('/GET/:id user', () => {
+        it('it should GET a user by the given id', (done) => {
+          let user = new User({
+            username: "fulanito",
+            first_name: "Fulano",
+            last_name: "de Tal"
+          });
+          user.save((err, user) => {
+              chai.request('http://localhost:3000')
+              .get('/users/' + user._id)
+              .send(user)
+              .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                done();
+              });
+          });
+  
+        });
+    });
+    
+    describe('/PUT/:id user', () => {
+        it('it should UPDATE an user given the id', (done) => {
+            let user = new User({
+                username: "fulanito",
+                first_name: "Fulano",
+                last_name: "de Tal"
+              });
+          user.save((err, user) => {
+                  chai.request('http://localhost:3000')
+                  .put('/users/' + user._id)
+                  .send({
+                    username: "perengano",
+                    first_name: "Perengano",
+                    last_name: "de Tal"
+                  })
+                  .end((err, res) => {
+                      res.should.have.status(200);
+                      res.body.should.be.a('object');
+                    done();
+                  });
+            });
+        });
+    });
+
+    describe('/DELETE/:id user', () => {
+        it('it should DELETE an user given the id', (done) => {
+            let user = new User({
+                username: "fulanito",
+                first_name: "Fulano",
+                last_name: "de Tal"
+              });
+          user.save((err, user) => {
+                 chai.request('http://localhost:3000')
+                  .delete('/users/' + user.id)
+                  .end((err, res) => {
+                      res.should.have.status(204);
+                    done();
+                  });
+            });
+        });
+    });
 });
